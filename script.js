dc8616e09139b56de873a9929fd449e63f667004
monikaserbista1 <msstrona@interia.pl>
2026-07-22 13:41:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -18,7 +18,7 @@
   }
 
   const core = document.createElement('script');
-  core.src = 'script-core.js?v=60.0.0';
+  core.src = 'script-core.js?v=61.0.0';
   core.async = false;
   document.head.appendChild(core);
 })();
